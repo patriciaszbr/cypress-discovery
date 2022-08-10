257d91b9ea0abbdadea78b09b8e4af2bddfa69c6
Patrícia Souza <patricia.sz.br@gmail.com>
2022-08-10 20:47:52 -0300

    Validando a busca do CEP

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,9 +1,9 @@
 describe('Cadastro', () => {
   it('Usuario deve se tornar um entregador', () => {
-    cy.viewport(1440, 900);
-    cy.visit('https://buger-eats.vercel.app');
+    cy.viewport(1440, 900)
+    cy.visit('https://buger-eats.vercel.app') 
 
-    cy.get('a[href="/deliver"]').click();
+    cy.get('a[href="/deliver"]').click()
     cy.get('#page-deliver form h1').should(
       'have.text',
       'Cadastre-se para  fazer entregas'
@@ -14,11 +14,24 @@ describe('Cadastro', () => {
       cpf: '0000002022',
       email: 'triciasz@gmail.com',
       whatsapp: '1199999999',
+      endereco: {
+        cep: '04534011',
+        rua: 'Rua Joaquim Floriano',
+        numero: '143',
+        complemento: 'Apt 701',
+        bairro: "Itaim Bibi",
+        cidade_uf: 'São Paulo/SP'
+      }
     };
 
-    cy.get('input[name="name"]').type(entregador.nome);
-    cy.get('input[name="cpf"]').type(entregador.cpf);
-    cy.get('input[name="email"]').type(entregador.email);
-    cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+    cy.get('input[name="name"]').type(entregador.nome)
+    cy.get('input[name="cpf"]').type(entregador.cpf)
+    cy.get('input[name="email"]').type(entregador.email)
+    cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+    cy.get('input[name="postalcode"').type(entregador.endereco.cep)
+    cy.get('input[type=button][value="Buscar CEP"]').click()
+    cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+    cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
   });
-});
+})
