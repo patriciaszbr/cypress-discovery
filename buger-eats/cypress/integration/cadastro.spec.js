3f447d98aec1332c44b1f2456de70049df1451f4
Patrícia Souza <patricia.sz.br@gmail.com>
2022-08-10 16:45:49 -0300

    Checkpoints na automacao web

---
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -0,0 +1,12 @@
+describe("Cadastro", () => {
+  it("Usuario deve se tornar um entregador", () => {
+    cy.viewport(1440, 900);
+    cy.visit("https://buger-eats.vercel.app");
+
+    cy.get('a[href="/deliver"]').click();
+    cy.get("#page-deliver form h1").should(
+      "have.text",
+      "Cadastre-se para  fazer entregas"
+    );
+  });
+});
