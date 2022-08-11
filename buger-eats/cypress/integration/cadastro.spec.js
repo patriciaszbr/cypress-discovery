c192a80ca2ac1a7adb414107b126d5597c3f89e8
Patrícia Souza <patricia.sz.br@gmail.com>
2022-08-10 21:57:08 -0300

    Upload de arquivos

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -22,7 +22,8 @@ describe('Cadastro', () => {
         bairro: "Itaim Bibi",
         cidade_uf: 'São Paulo/SP'
       },
-      metodo_entrega: "Bicicleta"
+      metodo_entrega: "Bicicleta",
+      cnh: 'cnh-digital.jpg'
     };
 
     cy.get('input[name="name"]').type(entregador.nome)
@@ -40,6 +41,8 @@ describe('Cadastro', () => {
     cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
 
     cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+
+    cy.get('input[accept^="image"]').attachFile('images/' + entregador.cnh)
 
   });
 })
